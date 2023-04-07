b571a34e86b012b9772ceebd56cdf332a76ca6e4
guillermo-st <guillermo_stancanelli@outlook.com>
2023-04-07 20:02:03 -0300

    Add handlers for admin register/login

---
+++ handlers/admin_login.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/fiufit/users/contracts"
+	"github.com/fiufit/users/usecases/accounts"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type AdminLogin struct {
+	admins accounts.AdminRegisterer
+	logger *zap.Logger
+}
+
+func (h AdminLogin) Handle() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req contracts.AdminLoginRequest
+		err := ctx.ShouldBindJSON(&req)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, contracts.FormatErrResponse(contracts.ErrBadRequest))
+			return
+		}
+
+		res, err := h.admins.Login(ctx, req)
+		if err != nil {
+			if errors.Is(err, contracts.ErrInvalidPassword) {
+				ctx.JSON(http.StatusUnauthorized, contracts.FormatErrResponse(contracts.ErrInvalidPassword))
+				return
+
+			} else if errors.Is(err, contracts.ErrUserNotFound) {
+				ctx.JSON(http.StatusNotFound, contracts.FormatErrResponse(contracts.ErrUserNotFound))
+				return
+			}
+
+			ctx.JSON(http.StatusInternalServerError, contracts.FormatErrResponse(contracts.ErrInternal))
+			return
+		}
+
+		ctx.JSON(http.StatusOK, contracts.FormatOkResponse(res))
+	}
+}
---
+++ handlers/admin_register.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/fiufit/users/contracts"
+	"github.com/fiufit/users/usecases/accounts"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type AdminRegister struct {
+	admins accounts.AdminRegisterer
+	logger *zap.Logger
+}
+
+func NewAdminRegister(admins accounts.AdminRegisterer, logger *zap.Logger) AdminRegister {
+	return AdminRegister{admins: admins, logger: logger}
+}
+
+func (h AdminRegister) Handle() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req contracts.AdminRegisterRequest
+		err := ctx.ShouldBindJSON(&req)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, contracts.FormatErrResponse(contracts.ErrBadRequest))
+			return
+		}
+
+		res, err := h.admins.Register(ctx, req)
+		if err != nil {
+			h.logger.Error("unable to register new administrator", zap.Error(err), zap.Any("request", req))
+			ctx.JSON(http.StatusInternalServerError, contracts.FormatErrResponse(contracts.ErrInternal))
+			return
+		}
+
+		ctx.JSON(http.StatusOK, contracts.FormatOkResponse(res))
+	}
+}
