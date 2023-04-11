98156ff27ae9dc8d33decb854d30e7b246c2e591
guillermo-st <guillermo_stancanelli@outlook.com>
2023-04-11 15:44:47 -0300

    Add ToView() methods for the user

--- models/users.go
+++ models/users.go
@@ -17,3 +17,24 @@ type User struct {
 	MainLocation      string    `gorm:"not null"`
 	Interests         []string  `gorm:"-"`
 }
+
+func (u User) ToPublicView() map[string]interface{} {
+	return map[string]interface{}{
+		"id":            u.ID,
+		"nickname":      u.Nickname,
+		"display_name":  u.DisplayName,
+		"is_male":       u.IsMale,
+		"is_verified":   u.IsVerifiedTrainer,
+		"main_location": u.MainLocation,
+	}
+}
+
+func (u User) ToPrivilegedView() map[string]interface{} {
+	userMap := u.ToPublicView()
+	userMap["creation_date"] = u.CreatedAt
+	userMap["birth_date"] = u.BornAt
+	userMap["height"] = u.Height
+	userMap["weight"] = u.Weight
+
+	return userMap
+}
