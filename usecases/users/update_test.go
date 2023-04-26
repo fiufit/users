5f59de3feef3484fe9d36565145e2a05414b8a68
guillermo-st <guillermo_stancanelli@outlook.com>
2023-04-26 17:48:20 -0300

    Add a couple missing test for update usecase

--- usecases/users/update_test.go
+++ usecases/users/update_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/fiufit/users/contracts"
 	"github.com/fiufit/users/contracts/users"
@@ -115,4 +116,70 @@ func TestUpdateUserOk(t *testing.T) {
 
 	//then
 	assert.NoError(t, err)
+}
+
+func TestPatchUserModelOk(t *testing.T) {
+	userRepo := new(mocks.Users)
+	ctx := context.Background()
+	userID := "h0l4"
+
+	outdatedUser := models.User{
+		ID:                userID,
+		Nickname:          "Arnold",
+		DisplayName:       "Arnold Schwarzenegger",
+		IsMale:            true,
+		BornAt:            time.Now(),
+		Height:            100,
+		Weight:            100,
+		IsVerifiedTrainer: false,
+		MainLocation:      "Argentina",
+		Interests:         nil,
+	}
+
+	isMale := false
+	req := users.UpdateUserRequest{
+		ID:           userID,
+		Nickname:     "Arnold2",
+		DisplayName:  "Arnie",
+		IsMale:       &isMale,
+		BirthDate:    time.Now().Add(1),
+		Weight:       200,
+		Height:       200,
+		MainLocation: "USA",
+	}
+
+	userRepo.On("GetByNickname", ctx, req.Nickname).Return(models.User{}, contracts.ErrUserNotFound)
+	userUc := NewUserUpdaterImpl(userRepo)
+	updatedUser, err := userUc.patchUserModel(ctx, outdatedUser, req)
+
+	assert.NoError(t, err)
+	assert.Equal(t, updatedUser.Nickname, req.Nickname)
+	assert.Equal(t, updatedUser.DisplayName, req.DisplayName)
+	assert.Equal(t, updatedUser.IsMale, *req.IsMale)
+	assert.Equal(t, updatedUser.BornAt, req.BirthDate)
+	assert.Equal(t, updatedUser.Weight, req.Weight)
+	assert.Equal(t, updatedUser.Height, req.Height)
+	assert.Equal(t, updatedUser.MainLocation, req.MainLocation)
+}
+
+func TestPatchUserModelRepoError(t *testing.T) {
+	userRepo := new(mocks.Users)
+	ctx := context.Background()
+	userID := "h0l4"
+
+	outdatedUser := models.User{
+		ID:       userID,
+		Nickname: "Arnold",
+	}
+
+	req := users.UpdateUserRequest{
+		ID:       userID,
+		Nickname: "Arnold2",
+	}
+
+	userRepo.On("GetByNickname", ctx, req.Nickname).Return(models.User{}, errors.New("repo error"))
+	userUc := NewUserUpdaterImpl(userRepo)
+	_, err := userUc.patchUserModel(ctx, outdatedUser, req)
+
+	assert.Error(t, err)
 }
